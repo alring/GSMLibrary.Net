d9aa29c7c8583b158524b1622ec5ed17d5ffd527
Vasiliev Nikita <sharpyfox@gmail.com>
2012-11-19 21:21:14 +0400

    Add Signal Quality Command

---
+++ GSMLibrary.Tests/Commands/SignalQualityCommandCases.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using GSMLibrary.Commands;
+
+namespace GSMLibrary.Tests.Commands.Commands
+{
+    public class SignalQualityCommandCases : BaseATCommandCase, IReadableCommandCases
+    {
+        private SignalQualityCommand _SignalCommand;        
+
+        public SignalQualityCommandCases()
+        {
+            _SignalCommand = new SignalQualityCommand();
+            _Command = _SignalCommand;
+        }
+
+        [Fact]
+        public void ParseCorrect()
+        {
+            bool zResult;
+
+            List<string> zList = new List<string>();
+
+            zList.Add("AT+CSQ");
+            zList.Add("+CSQ: 0,0");
+            zList.Add("OK");
+
+            zResult = _SignalCommand.Parse(zList);
+            Assert.Equal(true, zResult);
+            Assert.Equal(0, _SignalCommand.Quality);
+
+            zList.Clear();
+
+            zList.Add("AT+CSQ");
+            zList.Add("+CSQ: 24,0");
+            zList.Add("OK");
+
+            zResult = _SignalCommand.Parse(zList);
+            Assert.Equal(true, zResult);
+            Assert.Equal(77, _SignalCommand.Quality);
+
+            zList.Clear();
+
+            zList.Add("AT+CSQ");
+            zList.Add("+CSQ: 31,0");
+            zList.Add("OK");
+
+            zResult = _SignalCommand.Parse(zList);
+            Assert.Equal(true, zResult);
+            Assert.Equal(100, _SignalCommand.Quality);
+
+            zList.Clear();
+
+            zList.Add("AT+CSQ");
+            zList.Add("+CSQ: 35,0");
+            zList.Add("OK");
+
+            zResult = _SignalCommand.Parse(zList);
+            Assert.Equal(true, zResult);
+            Assert.Equal(100, _SignalCommand.Quality);
+        }
+
+        [Fact]
+        public void WrongPrefix()
+        {
+            List<string> zList = new List<string>();
+
+            zList.Add("+CS: 24,0");
+            zList.Add("OK");
+
+            Assert.False(_SignalCommand.Parse(zList));
+        }
+
+        [Fact]
+        public void WrongParamsCount()
+        {
+            List<string> zList = new List<string>();
+
+            zList.Add("+CSQ: 24, 0, 6");
+            zList.Add("OK");
+
+            Assert.False(_SignalCommand.Parse(zList));
+        }
+
+        [Fact]
+        public void WrongParams()
+        {
+            List<string> zList = new List<string>();
+
+            zList.Add("+CSQ: -89,0");
+            zList.Add("OK");
+
+            Assert.False(_SignalCommand.Parse(zList));
+
+            zList.Clear();
+            zList.Add("+CSQ: 25, 15");
+            zList.Add("OK");
+
+            Assert.False(_SignalCommand.Parse(zList));
+        }
+
+        [Fact]
+        public void ReadCommandTest()
+        {
+            Assert.Equal("AT+CSQ", _SignalCommand.ReadParamsCommand());
+        }
+    }
+}
---
+++ GSMLibrary/Commands/SignalQualityCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GSMLibrary.Commands
+{
+    public class SignalQualityCommand : BaseReadableCommand
+    {
+        private const int MINIMAL_DBM_QUALITY = -113;
+        private const int MAXIMUM_DBM_QUALITY = -51;
+        private const float DBM_TOPERCENT_COEFFICENT = 100F / ((-1) * MINIMAL_DBM_QUALITY - ((-1) * MAXIMUM_DBM_QUALITY) );
+
+        public SignalQualityCommand()
+        {
+            CommandPrefix = "CSQ";
+        }
+
+        private int _dbmQuality;
+        public int dbmQuality
+        {
+            get
+            {
+                return _dbmQuality;
+            }
+            private set
+            {
+                _dbmQuality = value;
+            }
+        }
+
+        public byte Quality
+        {
+            get
+            {   
+                return (byte) ((Math.Abs(MINIMAL_DBM_QUALITY) + _dbmQuality) * DBM_TOPERCENT_COEFFICENT);
+            }
+        }
+
+        public override bool Parse(List<string> aAnswer)
+        {   
+            if (base.Parse(aAnswer))
+            {
+                string[] zSplit = aAnswer[0].Split(new Char[] { ':', ',', ',' });
+                if (zSplit.Count() == 3)
+                {
+                    String _qualitystr = TrimValue(zSplit[1]);
+                    String _biterrorstr = TrimValue(zSplit[2]);
+
+                    byte _qualityVal;
+                    byte _bitErrorVal;
+
+                    if (byte.TryParse(_qualitystr, out _qualityVal) && (byte.TryParse(_biterrorstr, out _bitErrorVal)))
+                    {   
+                        if ((_bitErrorVal > 7) && (_bitErrorVal != 99))
+                            return false;
+
+                        if (_qualityVal > 31)
+                            _qualityVal = 31;
+
+                        dbmQuality = MINIMAL_DBM_QUALITY + _qualityVal * 2;
+                        return true;
+                    }
+                    else
+                        return false;                    
+                }
+                else
+                {
+                    _logger.Debug("InCorrect Params Count: {0}", zSplit.Count());
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+        public override String ReadParamsCommand()
+        {
+            return String.Concat("AT+", CommandPrefix);
+        }
+    }
+}
